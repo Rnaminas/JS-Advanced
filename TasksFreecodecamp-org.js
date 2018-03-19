527c4c28d877822049d76ef814c6b456f3704a5c
Naminas Roman <r.naminas@gmail.com>
2018-03-19 16:33:41 +0200

    created new additional solutions tasks

--- TasksFreecodecamp-org.js
+++ TasksFreecodecamp-org.js
@@ -1,5 +1,5 @@
 
-// Task  5 - Find the Longest Word in a String Complete 
+// Task  5 FreecodeCamp - Find the Longest Word in a String Complete 
         function findLongestWord(str) {
             var words =str.split(' ');
             var bigString=words[0];
@@ -18,7 +18,7 @@
         console.log(findLongestWord("What is the average airspeed velocity of an unladen swallow"));
         console.log(findLongestWord("What if we try a super-long word such as otorhinolaryngology"));
 
-    //Task 6 - Title Case a Sentence
+    //Task 6 FreecodeCamp - Title Case a Sentence
 
         function titleCase(str) {
             var words=str.split(' ');
@@ -34,6 +34,60 @@
         console.log(titleCase("I'm a little tea pot"));
         console.log(titleCase("sHoRt AnD sToUt"));
         console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));
+
+
+
+
+
+
+// Task 1 - Дан массив arr. Найдите среднее арифметическое его элементов. 
+// Проверьте задачу на массиве с элементами 12, 15, 20, 25, 59, 79.
+
+
+function getAverageValue(Array){
+  var average=0; var i;
+    for(i =0;i<Array.length;i++){
+      average+=Array[i];
+    }
+  return average/Array.length;
+}
+
+var arr =[12, 15, 20, 25, 59, 79];
+console.log(getAverageValue(arr));
+
+
+// Task 2 - Дан массив с числами. Оставьте в нем только положительные числа. 
+// Затем извлеките квадратный корень из этих чисел 
+
+
+function getPositivArray(Array){
+	var PositivArray=[]; var j=0;
+	for (var i=0;i<Array.length; i++) {
+		if(Array[i]<0) continue; // если число меньше нуля переходим к следующей итерации
+		else if(Array[i]>0){
+			PositivArray[j]=Array[i];
+			j++;
+		}
+	}
+	return Array=PositivArray;	
+	}
+
+function getRoot(Array){ // берем корень для всего массива
+	var result=0;
+	for (var i=0;i<Array.length; i++) {
+		Array[i]=Math.sqrt(Array[i]);
+		}
+	
+
+	return Array;	
+	}
+
+
+var arr =[-12, 16, 25, -25, -59, 625];
+// console.log(arr);
+arr=getPositivArray(arr);
+arr=getRoot(arr);	
+// console.log(arr);
 
 
 // *************************************************
